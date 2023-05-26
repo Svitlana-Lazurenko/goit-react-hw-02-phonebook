ce685b12ad0de3d3aafe90f2f9146ef9b5f34879
Svitlana-Lazurenko <svitsvit222@gmail.com>
2023-05-26 09:18:21 +0300

    Create react-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { Block, MainTitle, Title } from './App.styled';
 
 class App extends Component {
   state = {
     contacts: [],
     filter: '',
-    name: '',
-    number: '',
+  };
+
+  addContact = (name, number) => {
+    const id = nanoid();
+    const contactObj = { id, name, number };
+    const contacts = this.state.contacts;
+
+    for (const contact of contacts) {
+      if (contact.name === name) {
+        alert(`${name} is already in contacts`);
+        return;
+      }
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [contactObj, ...contacts],
+    }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
   };
 
   getVisibleContacts = () => {
@@ -17,92 +45,22 @@ class App extends Component {
     );
   };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const id = nanoid();
-    const number = form.elements.number.value;
-    const name = form.elements.name.value;
-    const contactObj = { id, name, number };
-    const contacts = this.state.contacts;
-
-    for (const contact of contacts) {
-      if (contact.name === name) {
-        alert(`${name} is already in contacts`);
-        return;
-      }
-    }
-    this.setState(prevState => {
-      return { contacts: [...prevState.contacts, contactObj] };
-    });
-  };
-
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
   render() {
-    const { name, number, filter } = this.state;
+    const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
 
     return (
-      <>
-        {/* <div>
-  <h1>Phonebook</h1>
-  <ContactForm ... />
+      <Block>
+        <MainTitle>Phonebook</MainTitle>
+        <ContactForm onSubmit={this.addContact} />
 
-  <h2>Contacts</h2>
-  <Filter ... />
-  <ContactList ... />
-</div> */}
-
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.handleChange}
-          />
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Add contact</button>
-        </form>
-        <input
-          type="text"
-          name="filter"
-          value={filter}
-          onChange={this.handleChange}
-        ></input>
-        <ul>
-          {visibleContacts.map(contact => (
-            <li key={contact.id}>
-              {contact.name}: {contact.number}
-              <button
-                type="button"
-                onClick={() => this.deleteContact(contact.id)}
-              >
-                Delete contact
-              </button>
-            </li>
-          ))}
-        </ul>
-      </>
+        <Title>Contacts</Title>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </Block>
     );
   }
 }
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Block = styled.div``;
+
+export const MainTitle = styled.h1``;
+
+export const Title = styled.h2``;
